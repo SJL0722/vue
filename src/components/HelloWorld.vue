a05806da465d6192eda5b2e1582941d2fb1ee1bd
songjinling <you@example.com>
2017-11-08 20:30:33 +0800

    ff

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>狗狗是你的嘴好朋</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -25,7 +25,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '欢迎来到汪星球！'
     }
   }
 }
